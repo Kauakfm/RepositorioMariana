9b3e16295a8e340e5c4fec624138d094e778f988
Kaua Martins <kaua.martins@postall-log.com.br>
2024-04-03 15:26:33 -0300

    Alteração na pagina de erro

--- src/pages/Erro/index.js
+++ src/pages/Erro/index.js
@@ -4,7 +4,7 @@ import './erro.css';
 function Erro(){
   return(
     <div className="not-found">
-      <h1>404</h1>
+      <h1>415</h1>
       <h2>Pagina não encontrada!</h2>
       <Link to="/">Veja todos filmes!</Link>
     </div>
