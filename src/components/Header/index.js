02998a6b9aa59ad588ead3098776936644ae732d
Kaua Martins <kaua.martins@postall-log.com.br>
2024-04-03 15:36:44 -0300

    alteracao no componente header da mensagem na tela

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Header(){
   return(
     <header>
-      <Link className="logo" to="/">Prime Flix</Link>
+      <Link className="logo" to="/">Netflix com a minha namorada linda e maravilhosa</Link>
       <Link className="favoritos" to="/favoritos">Meus filmes</Link>
     </header>
   )
